cbf5d54862946ff1f0810d370035c680c3d06535
ashishkrsingh-96613 <ashishk96613@gmail.com>
2025-04-13 06:51:33 +0000

    changes have been done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountID = 19562
+let accountemail = "ashishk02749@google.com"
+// variables can be changed futher
+// both let and var are the keyword of variable  
+var acountpassword = "10990"
+// sometimes in js we can use or assign the variables by not writing the var or let 
+accountcity = "jaipur"
+let accountstate ;
+// if we declare only variable but not assign it value js will give the value undefined 
+// accountID = 19582 
+// once aconst is declared it connot be changed in js
+
+accountemail = "harshitchaudhry@google.com"
+accountcity = "patna"
+accountpassword = "2348765"
+console.log(accountID);
+
+/* in js prefer not to use the variable ,(var)
+always prefer to use let 
+*/
+
+console.table([accountemail , acountpassword ,accountcity , accountstate]);
+
+// to print the value in table we simply use console.table  
